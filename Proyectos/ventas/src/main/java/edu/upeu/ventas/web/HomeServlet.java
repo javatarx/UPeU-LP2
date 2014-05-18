416b1826d01e611aee8f3224b118d770d313d93b
LC-PC <LC-PC@LC3-PC32>
2014-05-18 17:18:18 -0500

    Importacion inicial del proyecto

---
+++ Proyectos/ventas/src/main/java/edu/upeu/ventas/web/HomeServlet.java
@@ -0,0 +1,47 @@
+package edu.upeu.ventas.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HomeServlet
+ */
+public class HomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	public static final String VIEW_HOME = "/pages/home.jsp";
+
+	/**
+	 * Default constructor.
+	 */
+	public HomeServlet() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("HomeServlet.doGet()");
+		//
+		// PrintWriter out = response.getWriter();
+		// out.println("<b style='color:red;'>Hola</b> mundo");
+
+		request.getRequestDispatcher(VIEW_HOME).forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("HomeServlet.doPost()");
+	}
+
+}
